3b9f72bb181f92029b53c8551cf740f14c36bdb0
Alex <Sparknsk@users.noreply.github.com>
2024-10-21 03:23:55 +0700

    Test

--- bin/composer_paths.sh
+++ bin/composer_paths.sh
@@ -9,7 +9,7 @@ function test_composer {
 }
 
 function validate_composer {
-    "${php_path}" "${composer_path}" validate --no-check-publish --no-check-lock --working-dir "${working_directory}" > /dev/null 2>&1
+    "${php_path}" "${composer_path}" validate --no-check-publish --no-check-lock --working-dir "${working_directory}"
 }
 
 if ! test_composer; then
